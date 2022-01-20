6bff1c093f923f26f90a1c90de8d8032a9117d1d
0008mari <0008mari@gmail.com>
2022-01-21 01:29:43 +0900

    my first efcore app

--- EFCoreTutorials/Program.cs
+++ EFCoreTutorials/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace EFCoreTutorials
 {
@@ -7,6 +12,17 @@ namespace EFCoreTutorials
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Database=SchoolDB;Trusted_Connection=True;");
         }
     }
 }
@@ -23,3 +39,4 @@ public class Course
     public int CourseId { get; set; }
     public string CourseName { get; set; }
 }
+
