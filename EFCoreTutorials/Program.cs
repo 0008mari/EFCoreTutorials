b0cdcc9e80a636aab177af645c87fb9568770103
0008mari <0008mari@gmail.com>
2022-01-21 01:07:26 +0900

    초기 커밋.

---
+++ EFCoreTutorials/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EFCoreTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+
+}
+
+public class Course
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+}
